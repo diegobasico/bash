e53459500d3ceca41c64cfb134360b1ada7bcb31
diego <diego.ugaz13@gmail.com>
2025-07-13 14:28:13 -0500

    shell script works

--- set-hyprsunset.sh
+++ set-hyprsunset.sh
@@ -2,22 +2,15 @@
 
 hour=$(date +%H)
 on_hyprland=$(/usr/bin/pgrep -x Hyprland)
-active_hyprsunset=$(/usr/bin/pgrep -x hyprsunset)
 
 if [[ $on_hyprland ]]; then
-  if [[ $active_hyprsunset ]]; then
-    pkill -x hyprsunset
-    sleep 1
-  fi
 
   if ((7 <= hour && hour < 18)); then
-    hyprsunset --temperature 6500 &
-    brightnessctl set 100%
+    hyprctl hyprsunset temperature 6500
   elif ((18 <= hour && hour < 20)); then
-    hyprsunset --temperature 5000 &
-    brightnessctl set 70%
+    hyprctl hyprsunset temperature 5000
   else
-    hyprsunset --temperature 4200 &
-    brightnessctl set 30%
+    hyprctl hyprsunset temperature 4200
   fi
+
 fi
