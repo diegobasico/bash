724b5e2ebaeb77221820a02ebbcb661d19c2eebd
diego <diego.ugaz13@gmail.com>
2025-07-27 17:48:05 -0500

    installation to be tested

--- hyprsunset/install.sh
+++ hyprsunset/install.sh
@@ -9,3 +9,26 @@ BIN_PATH="$BIN_DIR/$BASENAME.sh"
 SYSTEMD_DIR="$HOME/.config/systemd/user"
 SERVICE_PATH="$SYSTEMD_DIR/$BASENAME.service"
 TIMER_PATH="$SYSTEMD_DIR/$BASENAME.timer"
+
+echo "Checking local bin and systemd folders..."
+
+mkdir -p "$BIN_DIR"
+mkdir -p "$SYSTEMD_DIR"
+
+echo "Copying files..."
+
+cp -v "$CURRENT_DIR/$BASENAME.sh" "$BIN_PATH"
+cp -v "$CURRENT_DIR/$BASENAME.service" "$SERVICE_PATH"
+cp -v "$CURRENT_DIR/$BASENAME.timer" "$TIMER_PATH"
+chmod +x "$BIN_PATH"
+
+echo "Reloading systemd daemon..."
+
+systemctl --user daemon-reexec
+systemctl --user daemon-reload
+
+echo "Enabling and starting the timer..."
+
+systemctl --user enable --now "$BASENAME.timer"
+
+echo "✔ Installation complete."
